2fae7d6f42d76d759af6c2baca611f6bbd2f8534
Bastalo <130367514+Bastalo@users.noreply.github.com>
2023-10-25 13:59:42 +0300

    Add files via upload

--- r2point.py
+++ r2point.py
@@ -55,7 +55,8 @@ class R2Point:
             k_1 = 0
             b_1 = p.x
         if k_1 != 0:
-            if y_min <= k_1 * x_max + b_1 <= y_max:
+            if y_min <= k_1 * x_max + b_1 <= y_max and \
+                    (p.x <= x_max <= q.x or q.x <= x_max <= p.x):
                 a = R2Point(x_max, k_1 * x_max + b_1)
                 if p.is_inside(k, m):
                     b = p
@@ -66,30 +67,42 @@ class R2Point:
                         b = R2Point((y_min - b_1)/k_1, y_min)
                     elif x_min <= (y_max - b_1)/k_1 <= x_max:
                         b = R2Point((y_max - b_1)/k_1, y_max)
-            elif y_min <= k_1 * x_min + b_1 <= y_max:
+            elif y_min <= k_1 * x_min + b_1 <= y_max and \
+                    (p.x <= x_min <= q.x or q.x <= x_min <= p.x):
                 a = R2Point(x_min, k_1 * x_min + b_1)
                 if p.is_inside(k, m):
                     b = p
                 else:
-                    # ()
                     if y_min <= k_1 * x_max + b_1 <= y_max:
                         b = R2Point(x_max, k_1 * x_max + b_1)
                     elif x_min <= (y_min - b_1)/k_1 <= x_max:
                         b = R2Point((y_min - b_1)/k_1, y_min)
                     elif x_min <= (y_max - b_1)/k_1 <= x_max:
                         b = R2Point((y_max - b_1)/k_1, y_max)
-            elif x_min <= (y_min - b_1)/k_1 <= x_max:
+            elif x_min <= (y_min - b_1)/k_1 <= x_max and \
+                    (p.y <= y_min <= q.y or q.y <= y_min <= p.y):
                 a = R2Point((y_min - b_1)/k_1, y_min)
                 if p.is_inside(k, m):
                     b = p
                 else:
                     if y_min <= k_1 * x_min + b_1 <= y_max:
                         b = R2Point(x_min, k_1 * x_min + b_1)
-                    # ()
                     elif y_min <= k_1 * x_max + b_1 <= y_max:
                         b = R2Point(x_max, k_1 * x_max + b_1)
                     elif x_min <= (y_max - b_1)/k_1 <= x_max:
                         b = R2Point((y_max - b_1)/k_1, y_max)
+            elif x_min <= (y_max - b_1)/k_1 <= x_max and \
+                    (p.y <= y_max <= q.y or q.y <= y_max <= p.y):
+                a = R2Point((y_max - b_1)/k_1, y_max)
+                if p.is_inside(k, m):
+                    b = p
+                else:
+                    if y_min <= k_1 * x_min + b_1 <= y_max:
+                        b = R2Point(x_min, k_1 * x_min + b_1)
+                    elif y_min <= k_1 * x_max + b_1 <= y_max:
+                        b = R2Point(x_max, k_1 * x_max + b_1)
+                    elif x_min <= (y_min - b_1)/k_1 <= x_max:
+                        b = R2Point((y_min - b_1)/k_1, y_min)
             d = a.dist(b)
         elif y_min <= p.y <= y_max and y_min <= q.y <= y_max or \
                 x_min <= p.x <= x_max and x_min <= q.x <= x_max:
@@ -131,17 +144,17 @@ class R2Point:
 
 if __name__ == "__main__":
     p, q, k, m = R2Point(
-        3.0, 3.0), R2Point(
-        3.0, -1.0), R2Point(
+        1.0, 1.0), R2Point(
+        0.0, 0.0), R2Point(
         0.0, 0.0), R2Point(
         2.0, 2.0)
     a, b, c = R2Point(
-        -1.0, -1.0), R2Point(
-        -1.0, 1.0), R2Point(
+        0.0, 1.0), R2Point(
+        -3.0, 0.0), R2Point(
         3.0, 1.0)
     print(R2Point.new_dist(p, q, k, m))
-    print(R2Point.new_dist(q, a, k, m))
-    print(R2Point.new_dist(a, b, k, m))
-    print(R2Point.new_dist(b, c, k, m))
+    print(R2Point.new_dist(q, b, k, m))
+    print(R2Point.new_dist(b, a, k, m))
+    print(R2Point.new_dist(a, p, k, m))
     print(R2Point.new_dist(c, p, k, m))
     print(R2Point.new_dist(c, q, k, m))
